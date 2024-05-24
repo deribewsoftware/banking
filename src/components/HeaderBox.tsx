27e9b12d6df9b8c7d2d33f6bd38537419a4bb22a
deribew <deribew.tech@gmail.com>
2024-05-24 12:50:40 -0700

    updated

---
+++ src/components/HeaderBox.tsx
@@ -0,0 +1,24 @@
+
+interface HeaderBoxProps{
+  type:string,
+  title:string,
+  subtext:string,
+  user:any
+}
+const HeaderBox = ({ type = "title", title, subtext, user }: HeaderBoxProps) => {
+  return (
+    <div className="header-box">
+      <h1 className="header-box-title">
+        {title}
+        {type === 'greeting' && (
+          <span className="text-bankGradient">
+            &nbsp;{user}
+          </span>
+        )}
+      </h1>
+      <p className="header-box-subtext">{subtext}</p>
+    </div>
+  )
+}
+
+export default HeaderBox
